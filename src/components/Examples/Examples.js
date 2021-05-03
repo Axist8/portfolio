3de8e9ba676732b40d6248575f82ff15c2644fe2
Axel Jorgensen <code@axeljorgensen.info>
2021-05-02 21:58:22 -0700

    piecing together jsx framing

---
+++ src/components/Examples/Examples.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Examples = () => {
+    return (
+        <section id="examples">
+            <div className="wrapper">
+                <div className="title">
+                    <h2>my portfolio</h2>
+                </div>
+                <div className="replacewithproject1name-wrapper">
+                    <p>project components here</p>
+                </div>
+                <div className="replacewithproject2name-wrapper">
+                    <p>etc..</p>
+                </div>
+            </div>
+        </section>
+    )
+}
